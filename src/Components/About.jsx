014d072b23f880d46db4106f5242f90413b09d37
Aaliyah Oladigbolu <100875512+Oladigbs18@users.noreply.github.com>
2024-01-06 13:20:57 +0100

    Revert "my lovely portfolio"
    This reverts commit 3f2b3d5a3eda23155e8b093068aa2de134f48599.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/istockphoto-501670801-612x612.jpg";
+import image from "../images/motion-background.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
